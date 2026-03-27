059abd3a7129c39d693494de49c23fdeaa532eca
Tetiana Shcherbyna <tshcherbinka@icloud.com>
2026-03-27 10:36:40 +0200

    feat: add base routes and constants

---
+++ app/locations/add/page.tsx
@@ -0,0 +1,3 @@
+export default function AddLocationPage() {
+  return <div>Add Location</div>;
+}
---
+++ lib/constants.ts
@@ -0,0 +1,8 @@
+export const ROUTES = {
+    HOME: '/',
+    LOGIN: '/login',
+    SIGNUP: '/signup',
+    LOCATIONS: '/locations',
+    ADD_LOCATION: '/locations/add',
+    PROFILE: '/profile',
+};
