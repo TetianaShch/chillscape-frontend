70e2e9bd69e5c3ec084a4872dacf153e3049d047
Dmytro <dmytriy.litvin@gmail.com>
2026-03-31 18:32:40 +0300

    small fix icons

--- components/blocks/AdvantagesBlock/AdvantagesBlock.tsx
+++ components/blocks/AdvantagesBlock/AdvantagesBlock.tsx
@@ -4,19 +4,19 @@ import { Icon } from '@/components/ui/Icon/Icon';
 const advantages = [
   {
     id: 1,
-    icon: 'icon-check',
+    icon: 'icon-select-check-box',
     title: 'Реальні відгуки',
     text: 'Користувачі діляться чесними враженнями, щоб ви робили правильний вибір.',
   },
   {
     id: 2,
-    icon: 'icon-filter',
+    icon: 'icon-filter-alt',
     title: 'Зручні фільтри',
     text: 'Шукайте за типом локації, регіоном, наявністю зручностей та іншими критеріями.',
   },
   {
     id: 3,
-    icon: 'icon-users',
+    icon: 'icon-communication',
     title: 'Спільнота мандрівників',
     text: 'Додавайте власні улюблені місця та діліться своїми неймовірними знахідками.',
   },
