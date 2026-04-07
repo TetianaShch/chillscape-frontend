fda3f83b1aa74047405697181566cacb4ddd520f
Mykhailo Pyroh <mykhailopk@gmail.com>
2026-04-07 12:25:39 +0200

    fix conflicts

--- app/(main)/locations/[locationId]/@modal/add-review/page.tsx
+++ app/(main)/locations/[locationId]/@modal/add-review/page.tsx
@@ -3,15 +3,10 @@
 import { useParams, useRouter } from 'next/navigation';
 import AddReviewModal from '@/components/blocks/AddReviewModal/AddReviewModal';
 
-<<<<<<< HEAD
-export default async function AddReviewPage({ params }: Props) {
-  return <div></div>;
-=======
 export default function AddReviewPage() {
   const router = useRouter();
   const params = useParams();
   const locationId = params.locationId as string;
 
   return <AddReviewModal locationId={locationId} onClose={() => router.back()} />;
->>>>>>> 2df84bf182ad08bf465f4393a4a923a388fda7e1
 }
--- app/(main)/locations/[locationId]/@modal/auth-prompt/page.tsx
+++ app/(main)/locations/[locationId]/@modal/auth-prompt/page.tsx
@@ -1,9 +1,5 @@
 import AuthPromptModal from '@/components/blocks/AuthPromptModal/AuthPromptModal';
 
 export default function AuthPromptPage() {
-<<<<<<< HEAD
-  return <div></div>;
-=======
   return <AuthPromptModal />;
->>>>>>> 2df84bf182ad08bf465f4393a4a923a388fda7e1
 }
